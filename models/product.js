1ea8a3b1fb11295c8a1073cbb99c770400046bdf
piyushpatil4270 <piyushpatil4270@gmail.com>
2024-07-13 17:31:55 +0530

    added details functionality

---
+++ models/product.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const path = require('path');
+
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      } else {
+        try {
+          cb(JSON.parse(fileContent));
+        } catch (error) {
+          cb([])
+        }
+        
+      }
+    });
+  
+  
+};
+
+module.exports = class Product {
+  constructor(title, imageUrl, description, price) {
+    this.title = title;
+    this.imageUrl = imageUrl;
+    this.description = description;
+    this.price = price;
+  }
+
+  save() {
+    this.id=Math.ceil(Math.random()).toString()
+    getProductsFromFile(products => {
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
+  }
+
+  static fetchAll(cb) {
+    getProductsFromFile(cb);
+  }
+
+  static findById(id,cb){
+   getProductsFromFile((products)=>{
+    const product=products.find((prod)=>prod.id===id)
+     cb(product)
+   })
+  }
+};
